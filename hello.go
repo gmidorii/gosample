248a3dbb1e7a1d21026e408b1b7c8ab86043bf49
tagasouichiro <stitow14@gmail.com>
2016-12-06 00:31:01 +0900

    add Image

--- hello.go
+++ hello.go
@@ -4,9 +4,36 @@ import (
 	"fmt"
 	"net/http"
 	"math"
+	"image/color"
+	"image"
 )
 
 
+func main() {
+	fmt.Printf("hello world!")
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+
+	// Type
+	result := Sqrt(9.0)
+	fmt.Printf("result: %v \n", result)
+
+	// Image
+	m := Image{}
+	fmt.Printf(m.Bounds().String())
+}
+
+/**
+	Handler
+ */
+func handler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello world!")
+}
+
+/**
+ 	Type Error
+ */
 type ErrNegativeSqrt float64
 
 func (e *ErrNegativeSqrt) Error() string {
@@ -16,19 +43,6 @@ func (e *ErrNegativeSqrt) Error() string {
 func (e *ErrNegativeSqrt) String() string {
 	return fmt.Sprintf("%v", *e)
 }
-
-func handler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "hello world!")
-}
-
-func main() {
-	result := Sqrt(9.0)
-	fmt.Printf("result: %v \n", result)
-	fmt.Printf("hello world!")
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
-}
-
 func Sqrt(x ErrNegativeSqrt) ErrNegativeSqrt {
 	z := 1.0
 	zNew := 1.0
@@ -46,3 +60,22 @@ func Sqrt(x ErrNegativeSqrt) ErrNegativeSqrt {
 	return ErrNegativeSqrt(zNew)
 }
 
+/**
+	Image
+ */
+type Image struct {}
+
+func (im *Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (im *Image) Bounds() image.Rectangle {
+	return image.Rectangle{
+		image.Point{0, 0},
+		image.Point{200, 200},
+	}
+}
+
+func (im *Image) At(x, y int) color.Color {
+	return color.RGBA{uint8(x % 256), uint8(y % 256), 255, 255}
+}
