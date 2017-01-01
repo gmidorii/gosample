401be8d2d62725bb9c805db635d0b85dceb38371
tagasouichiro <stitow14@gmail.com>
2017-01-01 17:25:20 +0900

    add comma,ok idiom

--- subhello.go
+++ subhello.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"time"
 	"fmt"
+	"time"
 )
 
 type Test struct {
@@ -12,8 +12,8 @@ type Test struct {
 
 func main() {
 	/*
-	1. parallel exec
-	 */
+		1. parallel exec
+	*/
 	slice := make([]int, 100000000)
 	for i, _ := range slice {
 		slice[i] = i
@@ -24,8 +24,8 @@ func main() {
 	chanSum(slice)
 
 	/*
-	2. map
-	 */
+		2. map
+	*/
 	var value string
 	var ok bool
 	tMap := map[string]string{"1": "one", "2": "two"}
@@ -39,18 +39,28 @@ func main() {
 	fmt.Printf("%v\n", tMap)
 
 	/*
-	3. String()
-	 */
-	t := Test{num: 1, str:"TEST"}
+		3. String()
+	*/
+	t := Test{num: 1, str: "TEST"}
 	fmt.Printf("%v", t)
 
 	/*
-	4. array append
-	 */
+		4. array append
+	*/
 	x := []int{1, 2, 3}
 	y := []int{4, 5, 6}
 	x = append(x, y...) // "..." is necessary
 	fmt.Println(x)
+
+	/*
+		5. comma, ok idiom
+	*/
+	var v interface{} = 0
+	if str, ok := v.(string); ok {
+		fmt.Println(str)
+	} else {
+		fmt.Println("value is not string")
+	}
 }
 
 // String return format string
@@ -86,8 +96,8 @@ func chanSum(slice []int) {
 	c2slice := slice[len(slice)/2:]
 	Sum(c1, c1slice)
 	Sum(c2, c2slice)
-	sum += <- c1
-	sum += <- c2
+	sum += <-c1
+	sum += <-c2
 	fmt.Println(sum)
 	endT := time.Now()
 	fmt.Printf("%f sec \n", endT.Sub(startT).Seconds())
